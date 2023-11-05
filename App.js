705e8d573bc1d9cf1a9540883d7fcbb5bb20ab03
tejaspundpal <96496782+tejaspundpal@users.noreply.github.com>
2023-11-05 16:09:00 +0530

    Base app

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading1 = React.createElement("h1",{
+    id:"title-1",
+    className:"Heading-1",
+},"Heading 1 from parcel");
+
+const heading2 = React.createElement("h2",{
+    id:"title-2",
+    className:"Heading-2",
+},"Heading 2");
+
+const container = React.createElement("div",{
+    className:"container",
+},[heading1,heading2]);
+//console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
+
+// normal javascript--->
+// var h1 = document.createElement("h1");
+// h1.innerHTML = "Tejas Pundpal";
+// document.getElementById("root").appendChild(h1);
